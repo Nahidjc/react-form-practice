191dcccb5b586e0b598adc18d40ce32a8ef19b13
Nahid2194 <nahidjc80@gmail.com>
2021-04-29 14:36:28 +0600

    control form

--- src/components/ControlForm.jsx
+++ src/components/ControlForm.jsx
@@ -1,32 +1,51 @@
 import React from "react";
 
 class ControlForm extends React.Component {
+  state = {
+    name: "",
+    email: "",
+    password: "",
+  };
+
   handleSubmit = (event) => {
     event.preventDefault();
-    const data = {};
-    data.name = event.target.name.value;
-    data.email = event.target.email.value;
-    data.password = event.target.password.value;
-    console.log(data);
+    console.log(this.state);
+    this.setState({
+      name: "",
+      email: "",
+      password: "",
+    });
+  };
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
   };
   render() {
+    const { name, email, password } = this.state;
     return (
       <form onSubmit={this.handleSubmit}>
         <input
           type="text"
           className="form-control"
           name="name"
+          value={name}
+          onChange={this.handleChange}
           placeholder="MD Nahid"
         />
         <input
           type="email"
           className="form-control"
           name="email"
+          onChange={this.handleChange}
+          value={email}
           placeholder="nahid@gmail.com"
         />
         <input
           type="password"
           className="form-control"
+          value={password}
+          onChange={this.handleChange}
           name="password"
           placeholder="******"
         />
