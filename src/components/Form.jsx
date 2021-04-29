4ff76372a772b75799262e7c83e369c8a53d085e
Nahid2194 <nahidjc80@gmail.com>
2021-04-29 11:46:27 +0600

    checkbox working

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,14 +6,21 @@ class Form extends React.Component {
     country: "",
     bio: "",
     birthDay: "",
+    gender: "",
+    agree:false,
   };
   handleChange = (event) => {
     this.setState({
       [event.target.name]: event.target.value,
     });
   };
+  handleCheckBox = (event) => {
+    this.setState({
+      agree: event.target.checked,
+    });
+  }
   render() {
-    const { name, country, bio, birthDay } = this.state;
+    const { name, country, bio, birthDay,agree } = this.state;
     return (
       <div className="mt-5">
         <input
@@ -52,6 +59,16 @@ class Form extends React.Component {
           type="date"
           name="birthDay"
         />
+        <div>
+            <input type="radio" className="ms-2" name='gender' value='Male'  onChange={this.handleChange} />Male
+            <input className="ml-2" type="radio" name='gender' value='Female'  onChange={this.handleChange} />Female
+            <input type="radio" className="ml-2" name='gender' value='Other'  onChange={this.handleChange}  />Other
+        </div>
+        <div className="">
+          <input type="checkbox" name="agree" checked={agree} onChange={this.handleCheckBox} />
+          I agree to all the terms and conditions
+        </div>
+        <br/>
 
         <button className="btn btn-primary" onClick={ ()=>console.log(this.state)}>
           Save Data
