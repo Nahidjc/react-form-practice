c9b4be594f0926556af056f109963ada2f86647d
Nahid2194 <nahidjc80@gmail.com>
2021-04-29 10:14:54 +0600

    form adding

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from './components/Form';
 function App() {
   return (
-    <div className="App">
-
+    <div className="container" >
+      <Form></Form>
     </div>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,26 +1,64 @@
-import React from 'react';
+import React from "react";
 
-class Form extends React.Component   {
-    state={
-        name:'',
-        country:'',
-        bio:'',
-        birthDay:'',
-    };
-    handleChange = event =>{
+class Form extends React.Component {
+  state = {
+    name: "",
+    country: "",
+    bio: "",
+    birthDay: "",
+  };
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+  render() {
+    const { name, country, bio, birthDay } = this.state;
+    return (
+      <div className="mt-5">
+        <input
+          className="form-control my-2 "
+          onChange={this.handleChange}
+          type="text"
+          name="name"
+          value={name}
+          placeholder="Md Nahid"
+        />
+        <select
+          className="form-control my-2"
+          name="country"
+          value={country}
+          onChange={this.handleChange}
+        >
+          <option>Select country</option>
+          <option value="Bangladesh">Bangladesh</option>
+          <option value="India">India</option>
+          <option value="Srilanka">Srilanka</option>
+          <option value="Pakistan">Pakistan</option>
+        </select>
 
-    };
-    render() {
-        return (
-            <div>
-                <input type="text"
-                name="name"
-                placeholder='Md Nahid'
-                onChange={th}/>
-            </div>
-        );
-    }
+        <textarea
+          className="form-control my-2"
+          onChange={this.handleChange}
+          name="bio"
+          value={bio}
+          placeholder="Tell Me About Yourself"
+        ></textarea>
 
-};
+        <input
+          className="form-control my-2"
+          onChange={this.handleChange}
+          value={birthDay}
+          type="date"
+          name="birthDay"
+        />
+
+        <button className="btn btn-primary" onClick={ ()=>console.log(this.state)}>
+          Save Data
+        </button>
+      </div>
+    );
+  }
+}
 
 export default Form;
